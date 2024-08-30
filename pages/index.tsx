5b2643180954d729a0a7268bcc17a60aa524e231
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-08-30 16:53:36 +0900

    [add]ServerSidePropsとLoaderの導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
+import 'semantic-ui-css/semantic.min.css';
+import { Loader } from "semantic-ui-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,24 +20,23 @@ interface IndexPageProps{
 initialCatImageUrl : string;
 }
 
+const fetchCatImage = async() :Promise<SeartchCatImage> =>{
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = await res.json();
+  // console.log(result[0]);
+  return result[0];
+};
 
+const Home: NextPage<IndexPageProps> = ({initialCatImageUrl}) => {
 
-
-export default function Home() {
-
-  const [catImageUrl, setCatImageUrl] = useState("");
-
-
-  const fetchCatImage = async() :Promise<SeartchCatImage> =>{
-    const res = await fetch("https://api.thecatapi.com/v1/images/search");
-    const result = await res.json();
-    // console.log(result[0]);
-    return result[0];
-  };
+  const [catImageUrl, setCatImageUrl] = useState("initialCatImageUrl");
+  const [isLoading, setIsLoading]= useState(false);
 
   const handleClick = async() => {
+    setIsLoading(true);
     const catImage = await fetchCatImage();
     setCatImageUrl(catImage.url);
+    setIsLoading(false);
   };
 
   return (
@@ -48,9 +49,11 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
       <h1>猫画像アプリ</h1>
-      <div className={styles.img}>
+      {isLoading ? (<Loader active/>
+    ) : (<div className={styles.img}>
       <img src={catImageUrl}/>
-      </div>
+      </div>)}
+      
       <button className={styles.btn} onClick={handleClick}>
         今日の猫さん
         </button>
@@ -59,4 +62,15 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps<IndexPageProps>;
+export const getServerSideProps: GetServerSideProps<
+IndexPageProps
+>= async () => {
+  const catImage = await fetchCatImage();
+  return{
+    props: {
+      initialCatImageUrl: catImage.url,
+    },
+  };
+};
+
+export default Home;
